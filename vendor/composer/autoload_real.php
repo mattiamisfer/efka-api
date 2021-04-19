fba828b4fb415f7fdbf0382cdc913e91288d694d
mattiamisfer <mattiamisfer@gmail.com>
2021-04-19 22:18:36 +0530

    jetstream added

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit3b55a59bff2ba60402dfe0888357cef2
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit3b55a59bff2ba60402dfe0888357cef2', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit3b55a59bff2ba60402dfe0888357cef2', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit3b55a59bff2ba60402dfe0888357cef2::getInitializer($loader));
         } else {
